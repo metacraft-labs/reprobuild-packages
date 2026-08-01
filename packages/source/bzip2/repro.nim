855ba6afd58dbab09e4399981f62c9f58f02cfcf
Zahary Karadjov <zahary@metacraft-labs.com>
2026-08-02 01:51:48 +0300

    Stage the source bzip2 command suite

--- packages/source/bzip2/repro.nim
+++ packages/source/bzip2/repro.nim
@@ -31,7 +31,7 @@ package bzip2Source:
     setCurrentOwningPackageOverride("bzip2Source")
     try:
       let patches = @[
-        "printf '\n.PHONY: repro_install\nrepro_install:\n\t$(MAKE) -f Makefile-libbz2_so clean\n\t$(MAKE) -f Makefile-libbz2_so\n\t$(MAKE) PREFIX=/usr DESTDIR=$(DESTDIR) install\n\tmkdir -p $(DESTDIR)/usr/lib\n\tcp -a libbz2.so.1.0.8 $(DESTDIR)/usr/lib/\n\tln -sfn libbz2.so.1.0.8 $(DESTDIR)/usr/lib/libbz2.so.1.0\n\tln -sfn libbz2.so.1.0.8 $(DESTDIR)/usr/lib/libbz2.so.1\n\tln -sfn libbz2.so.1.0.8 $(DESTDIR)/usr/lib/libbz2.so\n' >> ./src/Makefile",
+        "printf '\n.PHONY: repro_install\nrepro_install:\n\t$(MAKE) -f Makefile-libbz2_so clean\n\t$(MAKE) -f Makefile-libbz2_so\n\tmkdir -p $(DESTDIR)/usr/bin $(DESTDIR)/usr/include $(DESTDIR)/usr/lib\n\tcp -a bzip2 bzip2recover bzgrep bzmore bzdiff $(DESTDIR)/usr/bin/\n\tln -sfn bzip2 $(DESTDIR)/usr/bin/bunzip2\n\tln -sfn bzip2 $(DESTDIR)/usr/bin/bzcat\n\tln -sfn bzgrep $(DESTDIR)/usr/bin/bzegrep\n\tln -sfn bzgrep $(DESTDIR)/usr/bin/bzfgrep\n\tln -sfn bzmore $(DESTDIR)/usr/bin/bzless\n\tln -sfn bzdiff $(DESTDIR)/usr/bin/bzcmp\n\tcp -a bzlib.h $(DESTDIR)/usr/include/\n\tcp -a libbz2.a libbz2.so.1.0.8 $(DESTDIR)/usr/lib/\n\tln -sfn libbz2.so.1.0.8 $(DESTDIR)/usr/lib/libbz2.so.1.0\n\tln -sfn libbz2.so.1.0.8 $(DESTDIR)/usr/lib/libbz2.so.1\n\tln -sfn libbz2.so.1.0.8 $(DESTDIR)/usr/lib/libbz2.so\n' >> ./src/Makefile",
       ]
       let pkg = autotools_package(
         srcDir = "./src",
