17aea3d57aee6b503ebeec0d6c99daa013081181
Zahary Karadjov <zahary@metacraft-labs.com>
2026-08-16 05:56:41 +0300

    Limit gettext runtime path to private libraries

--- packages/source/gettext/repro.nim
+++ packages/source/gettext/repro.nim
@@ -248,9 +248,7 @@ package gettextSource:
         configureOptions = opts,
         allowSourceWrites = true,
         extraEnv = @[
-          ("NIX_LDFLAGS", ""),
           ("LDFLAGS", "-Wl,-rpath," &
-            sourcePackageInstallPath("glibc", "usr", "lib64") & ":" &
             sourcePackageInstallPath("gettext", "usr", "lib")),
         ])
       discard pkg.executable("msgfmt")
